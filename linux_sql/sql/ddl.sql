ae7077f614713efd86a2c48a600e3b6bede53b18
Hazem0012 <hazemalsagheer0012@gmail.com>
2026-04-08 03:05:29 +0000

    Fixed the formatting of ddl.sql

--- linux_sql/sql/ddl.sql
+++ linux_sql/sql/ddl.sql
@@ -2,17 +2,17 @@
 
 CREATE TABLE PUBLIC.host_info 
 (
-id      SERIAL NOT NULL,
-hostname   VARCHAR NOT NULL,
-cpu_number  INT2 NOT NULL,
-cpu_architecture  VARCHAR NOT NULL,
-cpu_model   VARCHAR NOT NULL,
-cpu_mhz   FLOAT8 NOT NULL,
-l2_cache  INT4 NOT NULL,
-timestamp TIMESTAMP NULL,
-total_mem  INT4 NULL,
-CONSTRAINT host_info_pk PRIMARY KEY (id),
-CONSTRAINT host_info_un UNIQUE (hostname));
+	id      SERIAL NOT NULL,
+	hostname   VARCHAR NOT NULL,
+	cpu_number  INT2 NOT NULL,
+	cpu_architecture  VARCHAR NOT NULL,
+	cpu_model   VARCHAR NOT NULL,
+	cpu_mhz   FLOAT8 NOT NULL,
+	l2_cache  INT4 NOT NULL,
+	"timestamp"	TIMESTAMP NULL,
+	total_mem  INT4 NULL,
+	CONSTRAINT host_info_pk PRIMARY KEY (id),
+	CONSTRAINT host_info_un UNIQUE (hostname));
 
 
 CREATE TABLE PUBLIC.host_usage 
