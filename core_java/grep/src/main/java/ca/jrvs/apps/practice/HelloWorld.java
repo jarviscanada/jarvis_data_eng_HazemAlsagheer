f261ddf8bc45034bdbe24a7e8121aeea29f044d1
Hazem0012 <hazemalsagheer0012@gmail.com>
2026-05-03 22:57:22 +0000

    Used grep for practice

---
+++ core_java/grep/src/main/java/ca/jrvs/apps/practice/HelloWorld.java
@@ -0,0 +1,12 @@
+package ca.jrvs.apps.practice;
+
+import java.util.ArrayList;
+
+class HelloWorld{
+
+  public static void main(String[] args) {
+    ArrayList<String> list = new ArrayList<>();
+    list.add("Hello");
+    System.out.println(list.get(0));
+  }
+}
